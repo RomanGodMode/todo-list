ae9c26fbbd59e412383fadc25917c215b0fcc279
RomanGodMode <roma.doter@yandex.ru>
2021-03-30 22:43:35 +0300

    scaffolded postgres db

---
+++ ormconfig.js
@@ -0,0 +1,30 @@
+const process = require('process');
+
+const username = process.env.POSTGRES_USER || 'postgres'
+const password = process.env.POSTGRES_PASSWORD || 'example'
+
+console.log(username)
+console.log(password)
+
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username,
+  password,
+  database: 'postgres',
+  synchronize: true,
+  dropSchema: false,
+  logging: true,
+  entities: [
+    __dirname + '/src/**/*.entity.ts',
+    __dirname + '/dist/**/*.entity.js',
+  ],
+  migrations: ['migrations/**/*.ts'],
+  subscribers: ['subscriber/**/*.ts', 'dist/subscriber/**/.js'],
+  cli: {
+    entitiesDir: 'src',
+    migrationsDir: 'migrations',
+    subscribersDir: 'subscriber',
+  },
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+const username = process.env.POSTGRES_USER || 'postgres'
+const password = process.env.POSTGRES_PASSWORD || 'postgres'
+//TODO: Заменить на инжекш через конфиг модуль
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username,
+      password,
+      database: 'postgres',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
