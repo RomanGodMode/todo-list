3aa1e21fbe9d89d2bedd4cb59e630f0f940c9163
RomanGodMode <roma.doter@yandex.ru>
2021-04-15 13:58:59 +0300

    scaffolded basic dockerized angular-app

---
+++ src/angular-client/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+import {environment} from '../environments/environment'
+
+// const igor = process.env.backendUrl
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-client'
+  back = environment.backendUrl
+}
---
+++ src/angular-client/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  backendUrl: `localhost:8080`,
+  reactAppUrl: `localhost:3000`
+};
